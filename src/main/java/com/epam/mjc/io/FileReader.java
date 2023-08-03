37d2513f28a53d38949a3cc5fc364282f5124d81
m4loo <128167781+m4loo@users.noreply.github.com>
2023-08-03 19:46:26 +0600

    Revert "done"
    This reverts commit a09224d6cb5003a5c314abe9a86818fcfd783c07.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,49 +1,11 @@
 package com.epam.mjc.io;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        String name = null;
-        int age = 0;
-        String email = null;
-        long phone = 0;
-
-        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(": ");
-                String key = parts[0];
-                String value = parts[1];
-                if (parts.length ==2) {
-                    switch (key) {
-                        case "Name" -> name = value;
-                        case "Age" -> {
-                            try {
-                                age = Integer.parseInt(value);
-                            } catch (NumberFormatException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        case "Email" -> email = value;
-                        case "Phone" -> {
-                            try {
-                                phone = Long.parseLong(value);
-                            } catch (NumberFormatException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-            return null;
-        }
-        return new Profile(name, age, email, phone);
+        return new Profile();
     }
 }
