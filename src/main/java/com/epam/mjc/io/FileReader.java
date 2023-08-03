c298fde8f8447bd5f8ddee17862d089d008668c6
m4loo <128167781+m4loo@users.noreply.github.com>
2023-08-03 19:47:33 +0600

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,62 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        int age = 0;
+        String email = null;
+        long phone = 0;
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new java.io.FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(": ");
+                String key = parts[0];
+                String value = parts[1];
+                if (parts.length == 2) {
+                    switch (key) {
+                        case "Name":
+                            name = value;
+                            break;
+                        case "Age":
+                            try {
+                                age = Integer.parseInt(value);
+                            } catch (NumberFormatException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case "Email":
+                            email = value;
+                            break;
+                        case "Phone":
+                            try {
+                                phone = Long.parseLong(value);
+                            } catch (NumberFormatException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return new Profile(name, age, email, phone);
     }
 }
