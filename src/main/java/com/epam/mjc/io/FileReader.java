d798b04bb0e604fb95e48d92d56accb061ffcda3
m4loo <128167781+m4loo@users.noreply.github.com>
2023-08-03 19:46:04 +0600

    Revert "done"
    This reverts commit adf8b2d5233e7dc2ed497f09c9a735147a5bef6a.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 
+
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
@@ -12,52 +13,37 @@ public class FileReader {
         String email = null;
         long phone = 0;
 
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new java.io.FileReader(file));
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 String[] parts = line.split(": ");
                 String key = parts[0];
                 String value = parts[1];
-                if (parts.length == 2) {
+                if (parts.length ==2) {
                     switch (key) {
-                        case "Name":
-                            name = value;
-                            break;
-                        case "Age":
+                        case "Name": name = value;
+                        case "Age": {
                             try {
                                 age = Integer.parseInt(value);
                             } catch (NumberFormatException e) {
                                 e.printStackTrace();
                             }
-                            break;
-                        case "Email":
-                            email = value;
-                            break;
-                        case "Phone":
+                        }
+                        case "Email": email = value;
+                        case "Phone": {
                             try {
                                 phone = Long.parseLong(value);
                             } catch (NumberFormatException e) {
                                 e.printStackTrace();
                             }
-                            break;
+                        }
                     }
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
             return null;
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
         return new Profile(name, age, email, phone);
     }
 }
